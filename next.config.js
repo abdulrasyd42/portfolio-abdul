afcb398d91c58f58f84bca1fd6d8677bdbcb327e
abdulrasyd42 <abdulrasyd42@gmail.com>
2026-04-18 14:48:44 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     // Tidak perlu remotePatterns lagi kalau semua gambar lokal
     // Tapi kalau masih mau pakai campuran (lokal + eksternal), 
     // biarkan saja seperti semula
+    output: 'export',
+    basePath: '/portfolio-abdul',
+    assetPrefix: '/portfolio-abdul/',
+    trailingSlash: true,
   },
 }
 
