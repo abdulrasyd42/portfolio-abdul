245fe223034554177b2ec609fde098f7429c2fce
abdulrasyd42 <abdulrasyd42@gmail.com>
2026-04-25 15:13:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     // Tidak perlu remotePatterns lagi kalau semua gambar lokal
-//     // Tapi kalau masih mau pakai campuran (lokal + eksternal), 
-//     // biarkan saja seperti semula
-//     output: 'export',
-//     basePath: '/portfolio-abdul',
-//     assetPrefix: '/portfolio-abdul/',
-//     trailingSlash: true,
-//   },
-// }
-
-// module.exports = nextConfig
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // WAJIB untuk GitHub Pages
+  output: 'export',
   basePath: '/portfolio-abdul',
   assetPrefix: '/portfolio-abdul/',
   trailingSlash: true,
 
   images: {
-    unoptimized: true, // penting untuk next/image di static export
+    unoptimized: true,
   },
 }
 
